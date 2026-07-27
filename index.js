9635a39014169bffc5c98b072869baa015e77ed6
YUVRAJSINGH1116 <yuvrajsingh115a@gmail.com>
2026-07-27 15:41:14 +0530

    if else condiion

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello....Using JS")
+// console.log("Hello....Using JS")
+
+function sum(a,b){
+    return a+b;
+}
+console.log(sum(23, 30));
+
+let asqrt = Math.sqrt(20)+Math.sqrt(30);
+console.log(asqrt);
+
+let a =34;
+if(a>20){
+    let a=45;
+    console.log("a inside a="+a)
+}
+console.log("a outside a="+a)
